0c3f4dd5119930068e09e58d6938e11abc720bdd
vikramdev772 <vikramdev772@gmail.com>
2026-02-02 11:02:12 +0530

    router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './page/Home'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
     <>
-      <Home/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/home' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
